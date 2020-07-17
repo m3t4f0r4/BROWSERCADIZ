d6022252541c1bb8d34cf54a2ac7d490256b50fd
m3t4f0r4 <40468288+m3t4f0r4@users.noreply.github.com>
2020-07-17 14:27:59 +0200

    Create browsercadiz.py

---
+++ browsercadiz.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# BrowserCadiz 1.0.0 - Copyright (c) @m3t4f0r4
+# Created by @m3t4f0r4
+# Enjoy it bro
+
+try:
+    url = sys.argv[1]
+except:
+    print("pon url bro")
+try:
+    os.system("firefox " + url + " &")
+except:
+    print("errooor!")
+
+print("URL abierta! Disfruta bro")
